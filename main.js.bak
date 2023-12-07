9897cb04852fb751d1d18d69926ee514b7867d1d
M-Abdullah03 <m18ab03@gmail.com>
2023-12-07 18:03:11 +0500

    Changed exec to execSync

--- main.js.bak
+++ main.js.bak
@@ -17,3 +17,4 @@ function testConditions(a, b, c) {
         return false;
     }
 }
+
--- statementCoverage.js
+++ statementCoverage.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+const { execSync, exec } = require('child_process');
 const fs = require('fs');
 const libCoverage = require('istanbul-lib-coverage');
 const esprima = require('esprima');
@@ -37,54 +37,49 @@ const getFunctionInfo = (filename) => {
 
 // // Run the nyc command
 const checkCoverage = () => {
-    exec('npx nyc --reporter=json --report-dir=./coverage node main.js', (error, stdout, stderr) => {
-        if (error) {
-            console.log(`Error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
+    try {
+        execSync('npx nyc --reporter=json --report-dir=./coverage node main.js')
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+        return;
+    };
 
-        // Read the coverage report
-        let coverageReport = JSON.parse(fs.readFileSync('./coverage/coverage-final.json', 'utf8'));
+    // Read the coverage report
+    let coverageReport = JSON.parse(fs.readFileSync('./coverage/coverage-final.json', 'utf8'));
 
-        // Create a coverage map
-        let coverageMap = libCoverage.createCoverageMap(coverageReport);
+    // Create a coverage map
+    let coverageMap = libCoverage.createCoverageMap(coverageReport);
 
-        // Output coverage map
-        //console.log(coverageMap.toJSON());
+    // Output coverage map
+    //console.log(coverageMap.toJSON());
 
-        // Calculate coverage summary
-        let executedLine = {};
-        let summary = libCoverage.createCoverageSummary();
-        coverageMap.files().forEach(function (f) {
-            let fc = coverageMap.fileCoverageFor(f);
-            summary.merge(fc.toSummary());
+    // Calculate coverage summary
+    let executedLine = {};
+    let summary = libCoverage.createCoverageSummary();
+    coverageMap.files().forEach(function (f) {
+        let fc = coverageMap.fileCoverageFor(f);
+        summary.merge(fc.toSummary());
 
-            // Get the line numbers that were executed
-            let executedLines = fc.getLineCoverage();
-            console.log(`Executed lines in ${f}:`, Object.keys(executedLines).filter(line => executedLines[line] > 0));
-            executedLine = executedLines;
-        });
-
-        // Output coverage summary
-        console.log(summary.toJSON());
-
-        let coverage = {
-            // "lines": summary.toJSON().lines.pct,
-            "statements": summary.toJSON().statements.pct,
-            // "functions": summary.toJSON().functions.pct,
-            // "branches": summary.toJSON().branches.pct
-            "lines": executedLine
-        };
-
-        fs.writeFileSync("coverage.json", JSON.stringify(coverage));
-
-        return summary.toJSON().statements.pct;
-
+        // Get the line numbers that were executed
+        let executedLines = fc.getLineCoverage();
+        console.log(`Executed lines in ${f}:`, Object.keys(executedLines).filter(line => executedLines[line] > 0));
+        executedLine = executedLines;
     });
+
+    // Output coverage summary
+    console.log(summary.toJSON());
+
+    let coverage = {
+        // "lines": summary.toJSON().lines.pct,
+        "statements": summary.toJSON().statements.pct,
+        // "functions": summary.toJSON().functions.pct,
+        // "branches": summary.toJSON().branches.pct
+        "lines": executedLine
+    };
+
+    fs.writeFileSync("coverage.json", JSON.stringify(coverage));
+
+    return summary.toJSON().statements.pct;
 };
 
 
@@ -100,9 +95,10 @@ const getCoverage = (functionName, paramsSet) => {
         fs.appendFileSync(fileName, functionCall);
     });
     checkCoverage();
+
     //restore file
     fs.copyFileSync(fileName + '.bak', fileName);
-    
+
 };
 
 
