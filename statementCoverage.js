df3076e639c7058faa2b51dd88f4e2e1d1b2ff93
M-Abdullah03 <m18ab03@gmail.com>
2023-12-07 17:24:31 +0500

    Added func

--- statementCoverage.js
+++ statementCoverage.js
@@ -15,7 +15,7 @@ function getFunctionInfo(filename) {
 
     // Traverse the AST
     estraverse.traverse(ast, {
-        enter: function(node) {
+        enter: function (node) {
             if (node.type === 'FunctionDeclaration' || node.type === 'FunctionExpression') {
                 // Get the function name and parameters length
                 const functionName = node.id ? node.id.name : 'anonymous function';
@@ -36,50 +36,54 @@ const functionInfo = getFunctionInfo('main.js');
 console.log(functionInfo);
 
 // // Run the nyc command
-exec('npx nyc --reporter=json --report-dir=./coverage node main.js', (error, stdout, stderr) => {
-    if (error) {
-        console.log(`Error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
+const getCoverage = () => {
+    exec('npx nyc --reporter=json --report-dir=./coverage node main.js', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`Error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
 
-    // Read the coverage report
-    let coverageReport = JSON.parse(fs.readFileSync('./coverage/coverage-final.json', 'utf8'));
+        // Read the coverage report
+        let coverageReport = JSON.parse(fs.readFileSync('./coverage/coverage-final.json', 'utf8'));
 
-    // Create a coverage map
-    let coverageMap = libCoverage.createCoverageMap(coverageReport);
+        // Create a coverage map
+        let coverageMap = libCoverage.createCoverageMap(coverageReport);
 
-    // Output coverage map
-    //console.log(coverageMap.toJSON());
+        // Output coverage map
+        //console.log(coverageMap.toJSON());
 
-    // Calculate coverage summary
-    let executedLine = {};
-    let summary = libCoverage.createCoverageSummary();
-    coverageMap.files().forEach(function (f) {
-        let fc = coverageMap.fileCoverageFor(f);
-        summary.merge(fc.toSummary());
+        // Calculate coverage summary
+        let executedLine = {};
+        let summary = libCoverage.createCoverageSummary();
+        coverageMap.files().forEach(function (f) {
+            let fc = coverageMap.fileCoverageFor(f);
+            summary.merge(fc.toSummary());
 
-        // Get the line numbers that were executed
-        let executedLines = fc.getLineCoverage();
-        console.log(`Executed lines in ${f}:`, Object.keys(executedLines).filter(line => executedLines[line] > 0));
-        executedLine = executedLines;
-    });
+            // Get the line numbers that were executed
+            let executedLines = fc.getLineCoverage();
+            console.log(`Executed lines in ${f}:`, Object.keys(executedLines).filter(line => executedLines[line] > 0));
+            executedLine = executedLines;
+        });
 
-    // Output coverage summary
-     console.log(summary.toJSON());
+        // Output coverage summary
+        console.log(summary.toJSON());
 
-     let coverage={
-        // "lines": summary.toJSON().lines.pct,
-        "statements": summary.toJSON().statements.pct,
-        // "functions": summary.toJSON().functions.pct,
-        // "branches": summary.toJSON().branches.pct
-        "lines": executedLine
-     };
+        let coverage = {
+            // "lines": summary.toJSON().lines.pct,
+            "statements": summary.toJSON().statements.pct,
+            // "functions": summary.toJSON().functions.pct,
+            // "branches": summary.toJSON().branches.pct
+            "lines": executedLine
+        };
 
         fs.writeFileSync("coverage.json", JSON.stringify(coverage));
-     
-});
+
+    });
+};
+
+
 
