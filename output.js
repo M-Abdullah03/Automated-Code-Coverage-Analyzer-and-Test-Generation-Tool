28689f7cb330efd577a04445da7a610d5e1133e7
ahmedkhan935 <ahmedkhanraza935@gmail.com>
2023-12-08 11:57:05 +0500

    trying branch

--- output.js
+++ output.js
@@ -1,45 +1,12 @@
 const fs = require("fs");
 let conditions = {};
-// Your function to test
-function testConditions(a, b, c) {
-    conditions.condition0 = {
-        "a == 0": a == 0,
-        "b == 1": b == 1,
-        "c == 2": c == 2
-    };
-
-    conditions.condition1 = {
-        "a == 1": a == 1,
-        "b == 2": b == 2,
-        "c == 3": c == 3
-    };
-
-    if(a == 0 && b == 1 || c == 2) {
-        conditions.condition0.condition0 = {
-            "a == 1": a == 1
-        };
-
-        if(a == 0) {
-            conditions.condition0.condition0.condition0 = {
-                "b == 2": b == 2
-            };
-
-            if(b == 2) {
-                return true;
-            }
-            return true;
-        }
-        return true;
+function validateNumbers(a, b, c) {
+    while(a>0)
+    {
+        a--;
     }
-    else if (a == 1 && b == 2 || c == 3) {
-        return true;
-    }
-     else {
-        return false;
-    }
+    
 }
-
-testConditions(0, 1, 2);
 
 fs.writeFileSync("conditions.json", JSON.stringify(conditions));
 
